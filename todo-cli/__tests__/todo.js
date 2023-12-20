65cb3abb1728544830d9cd20ec69105d79d48261
Anjali933 <151537476+Anjali933@users.noreply.github.com>
2023-12-20 16:48:13 +0530

    todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,84 +1,61 @@
-/* eslint-disable space-in-parens */
-/* eslint-disable object-curly-spacing */
-/* eslint-disable eol-last */
-/* eslint-disable padded-blocks */
-/* eslint-disable semi */
-/* eslint-disable indent */
-/* eslint-disable quotes */
-/* eslint-disable no-undef */
-const todoList = require("../todo");
-const { all, markAsComplete, add, overdue,
-    dueToday,
-    dueLater } = todoList();
-const today = new Date(); 
-const oneDay = 60 * 60 * 24 * 1000;
-describe("todoList", () => {
-  beforeAll(() => {
-    const today = new Date();
-     //referred to discord forum for this line of code
-    add({
-      title: "Test todo",
+const todolist = require("../todo");
+
+const {
+  all,
+  add,
+  markAsComplete,
+  overdue,
+  dueToday,
+  dueLater,
+  toDisplayableList,
+} = todolist;
+
+describe("Todolist Test", () => {
+  const Item = todolist();
+
+  test("should Add Todo item", () => {
+    const todoItemlength = Item.all.length;
+    // console.log(todoItemlength);
+    Item.add({
+      title: "Test Todo",
       completed: false,
-      dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
-        "en-CA",
-      ),
+      dueDate: new Date().toISOString().slice(0, 10),
     });
-    add({
-      title: "Test todo2",
-      completed: false,
-      dueDate: new Date(today.getTime() + 1 * oneDay).toLocaleDateString(
-        "en-CA",
-      ),
-    });
-    add({
-      title: "Test todo3",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
+    expect(Item.all.length).toBe(todoItemlength + 1);
   });
-  test("Should add new todo", () => {
-    const todoItemsCount = all.length;
-    add({
-      title: "Test todo",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
-    });
-    expect(all.length).toBe(todoItemsCount + 1);
-  });
+
   test("should mark a todo as complete", () => {
-    expect(all[0].completed).toBe(false);
-    markAsComplete(0);
-    expect(all[0].completed).toBe(true);
+    const todoItemlength = Item.all.length;
+    // console.log("todoItemlength" + todoItemlength);
+    expect(Item.all[todoItemlength - 1].completed).toBe(false);
+    Item.markAsComplete(todoItemlength - 1);
+    expect(Item.all[todoItemlength - 1].completed).toBe(true);
   });
-  test("checks return a list of overdue todos", () => {
-    const overDueTodoItemsCount =overdue().length;
-    add({
-        title: "Test todo",
-        completed: false,
-        dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
-          "en-CA",
-        ),
-      });
-    expect(overdue().length).toEqual(overDueTodoItemsCount+1) 
+
+  const today = new Date();
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  test("Retrieval of overdue Items", () => {
+    const todoItemlength = Item.all.length;
+    Item.overdue();
+    expect(todoItemlength).toBe(1);
   });
-  test("checks return a list of todos due today", () => {
-    const duetodayTodoItemsCount =dueToday().length;
-    add({
-        title: "Test todo3",
-        completed: false,
-        dueDate: new Date().toLocaleDateString("en-CA"),
-      });
-    expect(dueToday().length).toEqual(duetodayTodoItemsCount+1) ;
+
+  test("Retrieval of Due today Items", () => {
+    const todoItemlength = Item.all.length;
+    Item.dueToday();
+    expect(todoItemlength).toBe(1);
   });
-  test("checks return a list of todos due later", () => {
-    const dueLaterTodoItemsCount =dueLater().length;
-    add({
-        title: "Test todo2",
-        completed: false,
-        dueDate: new Date(today.getTime() + 2 * oneDay)
-        .toISOString()
-        .slice(0, 10),
-      });
-    expect(dueLater().length).toEqual(dueLaterTodoItemsCount+1);
+
+  test("Retrieval of dueLater Items", () => {
+    const todoItemlength = Item.all.length;
+    Item.dueLater();
+    expect(todoItemlength).toBe(1);
+  });
+
+  test("To Displayable List", () => {
+    const displayabletodo = Item.toDisplayableList(Item.all);
+    expect(typeof displayabletodo).toBe("string");
   });
 });
